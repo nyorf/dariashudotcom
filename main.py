1aa39f4e20ea73dd763ba17c91f7effb6c87fdb3
Dmitry Dolgov <me@nyorf.com>
2025-01-27 14:27:19 +0300

    switch order for robots.txt function

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import io
 
 app = FastAPI()
 
+@app.get(path="/robots.txt")
+@app.get(path="/robots")
+async def robots():
+    robots_file = io.FileIO('data/robots.txt', mode='r')
+    return StreamingResponse(content=robots_file, media_type="text/plain")
+
 
 @app.get(path="/")
 async def getWallpaper():
@@ -14,10 +20,3 @@ async def getWallpaper():
 @app.route("/{full_path:path}")
 async def catch_all(full_path: str):  # catch-all route, temporary as there's no content
     return RedirectResponse(url="https://dariashu.notion.site/f5f32e2f1f494086811bf2dad750cdba")
-
-
-@app.get(path="/robots.txt")
-@app.get(path="/robots")
-async def robots():
-    robots_file = io.FileIO('data/robots.txt', mode='r')
-    return StreamingResponse(content=robots_file, media_type="text/plain")
